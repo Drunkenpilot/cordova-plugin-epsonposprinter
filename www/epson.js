1287215b1fc8c912e784a9cff08cdafa942c6a71
Drunkenpilot <bobhao@gmail.com>
2016-09-29 00:04:13 +0200

    s

--- www/epson.js
+++ www/epson.js
@@ -3,7 +3,7 @@
 module.exports = {
   search : function(success_callback, error_callback ){
     cordova.exec(success_callback,error_callback,"EpsonPrinter","search",[]);
-  }
+  },
   check: function(){
     return "this is a test";
   }
