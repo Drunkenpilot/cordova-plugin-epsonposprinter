2137ecd839f7830075eabd80465ba41106902c8b
Drunkenpilot <bobhao@gmail.com>
2016-10-01 15:54:41 +0200

    update

--- src/android/ShowMsg.java
+++ src/android/ShowMsg.java
@@ -58,11 +58,11 @@ public class ShowMsg extends CordovaPlugin{
     show(msg, context);
   }
 
-  private static void show(String msg, Context context) {
-    AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+  private  void show(String msg, Context context) {
+
     cordova.getActivity().runOnUiThread(new Runnable() {
       public void run() {
-
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
         alertDialog.setMessage(msg);
         alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
           public void onClick(DialogInterface dialog, int whichButton) {
