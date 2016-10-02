ac727c1f7278f9e355c3184eb5107acb74f7f58e
Drunkenpilot <bobhao@gmail.com>
2016-10-02 18:53:06 +0200

    qq

--- www/epson.js
+++ www/epson.js
@@ -1,8 +1,8 @@
 /*global cordova, module*/
 
 module.exports = {
-  search : function(args,success_callback, error_callback ){
-      cordova.exec(success_callback,error_callback,"EpsonPrinter","search",[args]);
+  search : function(timeout,success_callback, error_callback ){
+      cordova.exec(success_callback,error_callback,"EpsonPrinter","search",[timeout]);
   },
   print : function(success_callback, error_callback ){
       cordova.exec(success_callback,error_callback,"EpsonPrinter","print",[]);
