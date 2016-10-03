a0edce524c775a806f1765e383136283422e7877
Drunkenpilot <bobhao@gmail.com>
2016-10-03 10:00:30 +0200

    a

--- src/android/HtmlToBitmap.java
+++ src/android/HtmlToBitmap.java
@@ -15,39 +15,41 @@ public class HtmlToBitmap{
 
 	public HtmlToBitmap( final Activity activity) {
 		activity.runOnUiThread(new Runnable() {
-		public void run(){
-			webView = new WebView(activity);
-			webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-			webView.setLayoutParams(
-			new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-			WebSettings settings = webView.getSettings();
-			settings.setBuiltInZoomControls(true);
-			settings.setUseWideViewPort(false);
-			settings.setJavaScriptEnabled(false);
-			settings.setSupportMultipleWindows(false);
+			public void run(){
+				webView = new WebView(activity);
+				webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+				webView.setLayoutParams(
+				new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+				WebSettings settings = webView.getSettings();
+				settings.setBuiltInZoomControls(true);
+				settings.setUseWideViewPort(false);
+				settings.setJavaScriptEnabled(false);
+				settings.setSupportMultipleWindows(false);
 
-			settings.setLoadsImagesAutomatically(true);
-			settings.setDomStorageEnabled(true);
-			settings.setLoadWithOverviewMode(true);
+				settings.setLoadsImagesAutomatically(true);
+				settings.setDomStorageEnabled(true);
+				settings.setLoadWithOverviewMode(true);
 
-		}
-	});
+			}
+		});
 	}
 
 
-	public Bitmap convert(String html) {
+	public Bitmap convert(String html,final Activity activity) {
+		activity.runOnUiThread(new Runnable() {
+			public void run(){
+				if (html == null || html.equals("")) {
+					return null;
+				}
+				Log.e("info", html);
+				webView.loadData(html, "text/html", "UTF8");
 
-		if (html == null || html.equals("")) {
-			return null;
-		}
-		Log.e("info", html);
-		webView.loadData(html, "text/html", "UTF8");
-
-		Bitmap bitmap = Bitmap.createBitmap(webView.getWidth(), webView.getHeight(), Config.RGB_565);
-		Canvas canvas = new Canvas(bitmap);
-		webView.draw(canvas);
-		return bitmap;
-
+				Bitmap bitmap = Bitmap.createBitmap(webView.getWidth(), webView.getHeight(), Config.RGB_565);
+				Canvas canvas = new Canvas(bitmap);
+				webView.draw(canvas);
+				return bitmap;
+			}
+		});
 	}
 
 }
