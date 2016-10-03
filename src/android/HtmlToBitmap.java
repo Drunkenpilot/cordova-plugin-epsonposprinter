535ef626c20318d68b3840995f15488483e6afd2
Drunkenpilot <bobhao@gmail.com>
2016-10-03 12:57:31 +0200

    a

--- src/android/HtmlToBitmap.java
+++ src/android/HtmlToBitmap.java
@@ -29,7 +29,7 @@ public class HtmlToBitmap{
 
 				webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
 				webView.setLayoutParams(
-				new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_PARENT, ViewGroup.LayoutParams.WRAP_PARENT));
+				new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 				WebSettings settings = webView.getSettings();
 				settings.setBuiltInZoomControls(true);
 				settings.setUseWideViewPort(false);
