65010f49624d3ec37b530dacae6932f5e53c26ce
Drunkenpilot <bobhao@gmail.com>
2016-10-05 23:16:09 +0200

    a

--- www/epson.js
+++ www/epson.js
@@ -6,15 +6,19 @@ module.exports = {
   },
   print : function(args,success_callback, error_callback ){
     args = angular.fromJson(args);
-    console.log(args);
-    console.log(args.printTemplate);
+    printContent = args.printContent;
+    printTemplate = args.printTemplate;
+    printMode = args.printMode;
+    printerSeries = args.printerSeries;
+    lang = args.lang;
+    printTarget = args.printTarget;
     // JSONArray explication
-    // 1. printContent JSONArray ex: [[{"name":"TextSize","value":"24"},{"name":"Text","value":"This is a test","newLine":true},{"name":"Line","value":180},{"name":"Align","value":"LEFT"}]]
+    // 1. printContent JSONArray ex: [{"name":"TextSize","value":"24"},{"name":"Text","value":"This is a test","newLine":true},{"name":"Line","value":180},{"name":"Align","value":"LEFT"}]
     // 2. printTemplate
     // 3. printMode
     // 4. printerSeries
     // 5. lang
     // 6. printTarget
-      cordova.exec(success_callback,error_callback,"EpsonPrinter","print",[[[{"name":"TextSize","value":"24"},{"name":"Text","value":"This is a test","newLine":true},{"name":"Line","value":180},{"name":"Align","value":"LEFT"}]],1,1,6,0,"USB:/dev/bus/usb/002/004"]);
+      cordova.exec(success_callback,error_callback,"EpsonPrinter","print",[[printContent],printTemplate,printMode,printerSeries,lang,printTarget]);
   }
 }
