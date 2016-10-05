1ef84acfa1ee06d3d1fb6153b7b1283107343aec
Drunkenpilot <bobhao@gmail.com>
2016-10-05 16:51:31 +0200

    a

--- www/epson.js
+++ www/epson.js
@@ -5,6 +5,6 @@ module.exports = {
       cordova.exec(success_callback,error_callback,"EpsonPrinter","search",[timeout]);
   },
   print : function(success_callback, error_callback ){
-      cordova.exec(success_callback,error_callback,"EpsonPrinter","print",[[[{"name":"TextSize","value":"24"},{"name":"Text","value":"This is a test","newLine":true},{"name":"Line","value":180},{"name":"Align","value":"LEFT"}]],[1]]);
+      cordova.exec(success_callback,error_callback,"EpsonPrinter","print",[[[{"name":"TextSize","value":"24"},{"name":"Text","value":"This is a test","newLine":true},{"name":"Line","value":180},{"name":"Align","value":"LEFT"}]],1]);
   }
 }
