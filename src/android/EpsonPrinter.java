4d6bfad909e0a56735e60508c319999121af59e7
Drunkenpilot <bobhao@gmail.com>
2016-10-05 13:35:26 +0200

    aa

--- src/android/EpsonPrinter.java
+++ src/android/EpsonPrinter.java
@@ -244,7 +244,7 @@ public class EpsonPrinter extends CordovaPlugin implements ReceiveListener {
 		//                 addParagraph();
 		//
 		// 								Bitmap testImg = receipt.build();
-		JSONArray html = [[{"name":"Text","value":"This is a test","newLine":true},{"name":"Line","value":180},{"name":"Align","value":"LEFT"}]];
+		JSONArray html = [{"name":"Text","value":"This is a test","newLine":true},{"name":"Line","value":180},{"name":"Align","value":"LEFT"}];
 		ReceiptBuilderExt receiptBuilder = new ReceiptBuilderExt(cordova.getActivity());
 		Bitmap testImg = receiptBuilder.build(html);
 
