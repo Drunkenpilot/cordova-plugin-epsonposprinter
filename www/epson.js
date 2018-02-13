268c3ef65c6295bbc784adf498cd2861b96392d0
Drunkenpilot <bobhao@gmail.com>
2018-02-13 18:57:58 +0100

    Revert "Revert "update""
    This reverts commit fefb124cb6999563cf264e096d4dcf748989517b.
    
    # Conflicts:
    #	www/epson.js

--- www/epson.js
+++ www/epson.js
@@ -30,4 +30,45 @@ var EpsonPrnter = {
         ]);
     }
 };
+<<<<<<< HEAD
+=======
+
+
+function EpsonPrnter() {}
+EpsonPrnter.prototype.search = function(timeout, success_callback, error_callback) {
+    exec(success_callback, error_callback, "EpsonPrinter", "search", [timeout]);
+};
+
+EpsonPrnter.prototype.print = function(args, success_callback, error_callback) {
+    args = angular.fromJson(args);
+    printContent = args.printContent;
+    printTemplate = args.printTemplate;
+    printMode = args.printMode;
+    printerSeries = args.printerSeries;
+    lang = args.lang;
+    printTarget = args.printTarget;
+    // JSONArray explication
+    // 1. printContent JSONArray ex: [{"name":"TextSize","value":"24"},{"name":"Text","value":"This is a test","newLine":true},{"name":"Line","value":180},{"name":"Align","value":"LEFT"}]
+    // 2. printTemplate
+    // 3. printMode
+    // 4. printerSeries
+    // 5. lang
+    // 6. printTarget
+    exec(success_callback, error_callback, "EpsonPrinter", "print", [
+        [printContent], printTemplate, printMode, printerSeries, lang, printTarget
+    ]);
+};
+
+EpsonPrnter.install = function() {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+
+    window.plugins.EpsonPrnter = new Inject();
+    return window.plugins.EpsonPrnter;
+};
+
+cordova.addConstructor(EpsonPrnter.install);
+
+>>>>>>> parent of c4a271b... Revert "update"
 module.exports = EpsonPrnter;
